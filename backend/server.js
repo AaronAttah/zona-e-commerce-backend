e85751e55a3686cc0930a1cf92b8fce9e34dc7cd
aarondebarron <aoahorizon@gmail.com>
2023-05-06 01:05:00 +0100

    refactored #4

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 // import data from "./data.js";
 import mongoose from "mongoose";
+import cors from "cors";
 import dotenv from "dotenv";
 import userRouter from "./routers/userRouter.js";
 import productRouter from "./routers/productRouter.js";
@@ -19,6 +20,9 @@ app.use((err, req, res, next) => {
 
 app.use(express.json({ limit: "50mb", exended: true }));
 app.use(express.urlencoded({ extended: true,  limit: "50mb"}));
+app.use(cors());
+// app.use(bodyParser.json());
+
 const URI = process.env.MONGODB_URL 
 // mongoose.connect(process.env.MONGODB_URL || "mongodb://127.0.0.1:27017/debarron", {
   mongoose.connect("mongodb+srv://devtest-db:password123_4@devtest-db.zx4g9a6.mongodb.net/devtest-db?retryWrites=true&w=majority", {
